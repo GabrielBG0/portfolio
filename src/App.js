1298eed00408806b1b77bbd6e9e16dbfd0d5d9ef
Gabriel B Gutierrez <gabriel.bgs00@gmail.com>
2022-05-15 15:35:31 -0400

    email hookedUp

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
+import { ParallaxProvider } from "react-scroll-parallax";
 
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
+      <ParallaxProvider>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </ParallaxProvider>
       <Footer />
     </div>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,20 @@
-import React from "react";
-import { FaAngleDown, FaCode, FaRobot, FaChartBar } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaAngleDown, FaCode, FaRobot, FaChartBar, FaCheck } from "react-icons/fa";
 import Scroll from "../../svg/Scroll.svg";
 import StarVertical from "../../svg/Star-Vertical370.svg";
 import charts from "../../svg/charts.svg";
 import { motion } from "framer-motion";
+import { Parallax } from "react-scroll-parallax";
+import * as emailjs from "emailjs-com";
+import { publicKey, templateId } from "../../keys";
 import './index.scss';
 
 export default function Home() {
+  const [fullName, setFullName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [message, setMessage] = useState('')
+  const [sent, setSent] = useState(false)
 
   const starVariants = {
     hidden: { opacity: 0, y: -100 },
@@ -27,25 +35,48 @@ export default function Home() {
     return window.innerWidth > 1099;
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
 
+    if (!(fullName !== '' && email !== '' && message !== '')) {
+      alert("Please fill out all fields")
+      return;
+    }
+
+
+    emailjs.sendForm('gmail', templateId, e.target, publicKey).then((result) => {
+      alert("Message Sent, I will get back to you shortly");
+      setSent(true)
+    }, (error) => {
+      alert("There was an error sending your message, please try again later");
+    });
+  }
   return (
     <>
       <div className="hero">
         <div className="left">
-          <motion.h1 initial='hidden' animate='visible' variants={scrollVariants} transition={{ duration: 1.5 }}>Software Engineering,<br />Machine Learning and<br />Data Science</motion.h1>
-          <motion.div initial='hidden' animate='visible' variants={isDescktop() ? scrollVariants : meetVariants} transition={{ duration: 1.5 }} className="meet">
-            <p>Meet Gabriel</p>
-            <motion.span animate={{ y: [0, -10, 0, -10, 0] }} transition={{ delay: 1.55 }} className="arrow"><FaAngleDown /></motion.span>
-          </motion.div>
-          <motion.img initial='hidden' animate='visible' variants={scrollVariants} transition={{ duration: 1.5 }} className="scroll" src={Scroll} alt='scroll' />
+          <Parallax className={{ marginTop: '3em' }} speed={-10}>
+            <motion.h1 initial='hidden' animate='visible' variants={scrollVariants} transition={{ duration: 1.5 }}>Software Engineering,<br />Machine Learning and<br />Data Science</motion.h1>
+            <motion.div initial='hidden' animate='visible' variants={isDescktop() ? scrollVariants : meetVariants} transition={{ duration: 1.5 }} className="meet">
+              <p>Meet Gabriel</p>
+              <motion.span animate={{ y: [0, -10, 0, -10, 0] }} transition={{ delay: 1.55 }} className="arrow"><FaAngleDown /></motion.span>
+            </motion.div>
+            <motion.img initial='hidden' animate='visible' variants={scrollVariants} transition={{ duration: 1.5 }} className="scroll" src={Scroll} alt='scroll' />
+          </Parallax>
         </div>
         <motion.div initial='hidden' animate='visible' variants={starVariants} transition={{ duration: 1.5 }} className="right">
           <img src={StarVertical} alt="Star" />
         </motion.div>
+
       </div>
       <section id="about" className="about">
         <div className="left">
-          <div className="inner">
+
+          <Parallax translateX={['-500px', '0px']}
+            easing="easeInQuad"
+            startScroll={100}
+            endScroll={800}
+          >
             <p className='subtitle'>
               about
             </p>
@@ -55,47 +86,60 @@ export default function Home() {
             <p className="text">
               I'm a software engineer and machine learning enthusiast.
             </p>
-          </div>
+          </Parallax>
         </div>
-        <div className="right">
+        <Parallax translateX={['500px', '0px']}
+          easing="easeInQuad"
+          startScroll={100}
+          endScroll={800}
+          className="right">
           <h2 className="title">My Interests</h2>
-        </div>
+        </Parallax>
       </section>
       <section id="skills" className="skills">
         <div className="skills-container">
-          <ul>
-            <li>
-              <div className="icon-container">
-                <FaCode />
-              </div>
-              <p className="title">Software<br />Engineering</p>
-              <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </li>
-            <li>
-              <div className="icon-container">
-                <FaRobot />
-              </div>
-              <p className="title">Machine<br />Learning</p>
-              <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </li>
-            <li>
-              <div className="icon-container">
-                <FaChartBar />
-              </div>
-              <p className="title">Data<br />Science</p>
-              <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </li>
-          </ul>
+          <Parallax
+            translateY={['-110px', '0px']}
+            easing="easeInQuad"
+            startScroll={900}
+            endScroll={1200}>
+            <ul>
+              <li>
+
+                <div className="icon-container">
+                  <FaCode />
+                </div>
+                <p className="title">Software<br />Engineering</p>
+                <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+              </li>
+              <li>
+                <div className="icon-container">
+                  <FaRobot />
+                </div>
+                <p className="title">Machine<br />Learning</p>
+                <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </li>
+              <li>
+                <div className="icon-container">
+                  <FaChartBar />
+                </div>
+                <p className="title">Data<br />Science</p>
+                <p className="desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              </li>
+            </ul>
+          </Parallax>
         </div>
       </section>
       <section id="projects" className="projects">
         <div className="projects-container">
           <div className="left">
-            <div className="inner">
+            <Parallax startScroll={1700}
+              endScroll={2000} speed={-5} className="inner">
               <p className="subtitle">Software Engineering</p>
               <p className="title">CookBook 🍜</p>
               <p className="desc">Software Engineering</p>
-            </div>
+            </Parallax>
           </div>
           <a href="https://github.com/GabrielBG0/CookBook"> <img className="right" src={charts} alt='charts' /></a>
         </div>
@@ -103,11 +147,12 @@ export default function Home() {
       <section className="projects">
         <div className="projects-container">
           <div className="left">
-            <div className="inner">
+            <Parallax startScroll={2200}
+              endScroll={3000} speed={-5} className="inner">
               <p className="subtitle">Data Science</p>
               <p className="title">Australian Fires 🔥</p>
               <p className="desc">Software Engineering</p>
-            </div>
+            </Parallax>
           </div>
           <a href="https://github.com/GabrielBG0/CookBook"> <img className="right" src={charts} alt='charts' /></a>
         </div>
@@ -115,11 +160,12 @@ export default function Home() {
       <section className="projects">
         <div className="projects-container">
           <div className="left">
-            <div className="inner">
+            <Parallax startScroll={2000}
+              endScroll={3200} speed={-5} className="inner">
               <p className="subtitle">Machine Learning</p>
               <p className="title">Smart Truncation Tool ✂</p>
               <p className="desc">Software Engineering</p>
-            </div>
+            </Parallax>
           </div>
           <a href="https://github.com/GabrielBG0/CookBook"> <img className="right" src={charts} alt='charts' /></a>
         </div>
@@ -127,6 +173,17 @@ export default function Home() {
       <div id="hire" className="action">
         <p className="like">Like What Your See?</p>
         <p className="hire"><u>Contact Me!</u></p>
+        <div className="contact">
+          <form onSubmit={handleSubmit}>
+            <input value={fullName} onChange={(e) => { setFullName(e.target.value) }} name='fullname' type="text" placeholder="Full Name*" />
+            <input value={email} onChange={(e) => { setEmail(e.target.value) }} name='email' type="email" placeholder="Email*" />
+            <input value={phone} onChange={(e) => { setPhone(e.target.value) }} name='phone' type="text" placeholder="Phone" />
+            <textarea value={message} onChange={(e) => { setMessage(e.target.value) }} name='message' placeholder="Message*"></textarea>
+          </form>
+          <div className="button-container">
+            <button className={{ cursor: sent ? 'default' : 'pointer' }} type={sent ? 'button' : 'submit'}>{sent ? <FaCheck /> : 'send'}</button>
+          </div>
+        </div>
       </div>
     </>
   );
