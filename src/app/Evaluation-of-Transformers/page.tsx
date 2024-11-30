912884bd2737e78d8d53895fc764ea18f2bb3b44
Gabriel Gutierrez <gabriel.bgs00@gmail.com>
2024-11-30 15:23:14 -0300

    Add redirect to GitHub repository for Evaluation of Transformers

---
+++ src/app/Evaluation-of-Transformers/page.tsx
@@ -0,0 +1,9 @@
+//redirect to https://github.com/GabrielBG0/Evaluation-of-Transformers-Seismic-Facies-Segmentation
+
+import { permanentRedirect } from "next/navigation";
+
+export default function RedirectToGithub() {
+  permanentRedirect(
+    "https://github.com/GabrielBG0/Evaluation-of-Transformers-Seismic-Facies-Segmentation"
+  );
+}
